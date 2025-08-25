b2d03ae8f345cf194f4fa3e1a4955d37b71f96c0
likanug <likanug.g@qq.com>
2025-08-25 12:34:15 +0800

    refactor: 重构项目的目录结构，拆分app模块到routes模块中

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,8 @@ use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment, WildcardSegment,
 };
+use crate::routes::home_page::HomePage;
+use crate::routes::not_found_page::NotFound;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -27,40 +29,5 @@ pub fn App() -> impl IntoView {
                 </Routes>
             </main>
         </Router>
-    }
-}
-
-/// Renders the home page of your application.
-#[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
-
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-    }
-}
-
-/// 404 - Not Found
-#[component]
-fn NotFound() -> impl IntoView {
-    // set an HTTP status code 404
-    // this is feature gated because it can only be done during
-    // initial server-side rendering
-    // if you navigate to the 404 page subsequently, the status
-    // code will not be set because there is not a new HTTP request
-    // to the server
-    #[cfg(feature = "ssr")]
-    {
-        // this can be done inline because it's synchronous
-        // if it were async, we'd use a server function
-        let resp = expect_context::<leptos_actix::ResponseOptions>();
-        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
-    }
-
-    view! {
-        <h1>"Not Found"</h1>
     }
 }
---
+++ src/routes/home_page.rs
@@ -0,0 +1,16 @@
+use leptos::prelude::ElementChild;
+use leptos::{component, view, IntoView};
+use leptos::prelude::{OnAttribute, RwSignal, Write};
+
+/// Renders the home page of your application.
+#[component]
+pub fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| *count.write() += 1;
+
+    view! {
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
